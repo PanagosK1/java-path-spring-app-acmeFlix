3c092bd314c80362393c96db5666892dd35aa947
PanagosK1 <hpanagos12@gmail.com>
2023-01-06 17:24:38 +0200

    Added Annotations for entity (Customers)

--- acmeFlix/src/main/java/gr/codelearn/spring/app/domain/Account.java
+++ acmeFlix/src/main/java/gr/codelearn/spring/app/domain/Account.java
@@ -1,38 +1,54 @@
 package gr.codelearn.spring.app.domain;
 
 import gr.codelearn.spring.app.domain.enumeration.Subscription;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.mapping.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import java.util.ArrayList;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 @SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Table(name = "ACCOUNTS")
-@SequenceGenerator(name = "idGenerator", sequenceName = "ACCOUNT_SEQ", initialValue = 1, allocationSize = 1)
+@Table(name = "ACCOUNTS", indexes = {@Index(columnList = "email")})
+@SequenceGenerator(name = "idGenerator", sequenceName = "CUSTOMERS_SEQ", initialValue = 1, allocationSize = 1)
 public class Account extends BaseModel {
-
-    @NotNull(message = "Account firstname")
-    @Column(length = 50, nullable = false, unique = true)
+    @NotNull(message = "Customer's FirstName cannot be null")
+    @Column(length = 50, nullable = false, unique = false)
     private String firstName;
+    @NotNull(message = "Customer's LastName cannot be null")
+    @Column(length = 50, nullable = false, unique = false)
     private String lastName;
+    @NotNull(message = "Customer's Password cannot be null")
+    @Column(length = 50, nullable = false, unique = false)
     private String passWord;
+    @NotNull(message = "Customer's FirstName cannot be null")
+    @Column(length = 50, nullable = false, unique = true)
     private String email;
-    private Subscription subscription;
 
+    @NotNull(message = "Subscription cannot be null")
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private Subscription subscription;
+    @NotNull(message = "Customer's FirstName cannot be null")
+    @Column(name = "AGE",length = 50, nullable = false, unique = true)
     @Min(18)
     @Max(100)
     private int Age;
 
-    private ArrayList<Profiles> profiles;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private List<Profiles> profiles;
 
+    public Account() {
+    }
 }
--- acmeFlix/src/main/java/gr/codelearn/spring/app/domain/Profiles.java
+++ acmeFlix/src/main/java/gr/codelearn/spring/app/domain/Profiles.java
@@ -1,9 +1,12 @@
 package gr.codelearn.spring.app.domain;
 
 import gr.codelearn.spring.app.domain.enumeration.Genre;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
 import java.util.ArrayList;
-
+@NoArgsConstructor
+@Entity
 public class Profiles extends BaseModel{
     private String username;
 
